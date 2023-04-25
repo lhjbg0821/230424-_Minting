197ade46158ad67b2fa5de57aa4328426e68e74e
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-25 10:12:15 +0900

    Minting URI까지 확인

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import Web3 from "web3";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./web3.config";
+
+const web3 = new Web3(window.ethereum);
+const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+
+function App() {
+  const [account, setAccount] = useState("");
+
+  const onClickAccount = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onClickMint = async () => {
+    try {
+      const mintNft = await contract.methods.mintNft().send({ from: account });
+
+      if (!mintNft) return;
+
+      const balanceOf = await contract.methods.balanceOf(account).call();
+
+      const tokenOfOwnerByIndex = await contract.methods
+        .tokenOfOwnerByIndex(account, parseInt(balanceOf) - 1)
+        .call();
+
+      const tokenURI = await contract.methods
+        .tokenURI(tokenOfOwnerByIndex)
+        .call();
+
+      console.log(tokenURI);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      {account ? (
+        <div>
+          {account.substring(0, 4)}...{account.substring(account.length - 4)}
+          <button onClick={onClickMint}>민팅</button>
+        </div>
+      ) : (
+        <button onClick={onClickAccount}>지갑로그인</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
